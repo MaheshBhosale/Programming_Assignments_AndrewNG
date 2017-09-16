59d52e0e461a88d39086ce879e40a849696ad338
Mahesh Bhosale <maheshbhosale24051996@gmail.com>
2017-09-16 16:20:38 +0530

    Adding Regularized cost function for logistic regression.

--- Week3/machine-learning-ex2/ex2/costFunctionReg.m
+++ Week3/machine-learning-ex2/ex2/costFunctionReg.m
@@ -6,10 +6,11 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 
 % Initialize some useful values
 m = length(y); % number of training examples
-
+n= length(theta);
 % You need to return the following variables correctly 
 J = 0;
 grad = zeros(size(theta));
+x=0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
@@ -17,11 +18,35 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+for i=1:m
+  s=sigmoid(X(i,:) * theta);
+  J += ((-y(i)*log(s)) -((1-y(i))*log(1-s)));
+ endfor
+J = J/m; 
+for j=2:n
+  
+  x = (x + (theta(j)*theta(j)));
+  endfor
 
+x = (( x * lambda ) /( 2 * m ));
+J = J + x;
 
+s=0;
+for i=1:m
+      s += (((sigmoid(X(i,:) * theta)) - y(i)) * X(i,1));
+  endfor
+  
+s = s/m;
+grad(1) = s;
+s = 0;
 
-
-
-% =============================================================
-
-end
+for j=2:n
+  for i=1:m
+      s += (((sigmoid(X(i,:) * theta))-y(i))*X(i,j));
+  endfor
+  s = s / m;
+  z = s + (( lambda / m ) * theta( j ));
+  grad( j ) = z;
+  s = 0;
+  z = 0;
+ endfor
