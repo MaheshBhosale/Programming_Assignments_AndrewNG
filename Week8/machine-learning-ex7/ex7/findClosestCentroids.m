2ea0901f683af0dc21d21e8252eb8cb523365082
MaheshBhosale <maheshbhosale24051996@gmail.com>
2017-12-30 18:20:56 +0530

    -Assigning data centroids

--- Week8/machine-learning-ex7/ex7/findClosestCentroids.m
+++ Week8/machine-learning-ex7/ex7/findClosestCentroids.m
@@ -20,14 +20,18 @@ idx = zeros(size(X,1), 1);
 %
 % Note: You can use a for-loop over the examples to compute this.
 %
-
-
-
-
-
-
-
+m = size(X,1);
+for i=1:m
+  dist = Inf;
+  cur_dist = 0;  
+  for j=1:K
+    cur_dist = sum((X(i,:) - centroids(j,:)).^2);
+    if cur_dist < dist
+      dist = cur_dist;
+      idx(i) = j;
+    endif
+  endfor
+endfor
 % =============================================================
-
 end
 
